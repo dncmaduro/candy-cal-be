e78622a52d6d9cc2d4c0abb4170e90063fd3dbbe
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2025-12-20 17:00:46 +0700

    feat: kpi (#95)

--- src/database/mongoose/schemas/LivestreamGoal.ts
+++ src/database/mongoose/schemas/LivestreamGoal.ts
@@ -3,14 +3,18 @@ import { Document, model, Schema, Types } from "mongoose"
 export interface LivestreamMonthGoal extends Document {
   month: number
   year: number
-  channel: string
+  channel: Types.ObjectId
   goal: number
 }
 
 export const LivestreamMonthGoalSchema = new Schema<LivestreamMonthGoal>({
   month: { type: Number, required: true },
   year: { type: Number, required: true },
-  channel: { type: String, required: true },
+  channel: {
+    type: Schema.Types.ObjectId,
+    ref: "livestreamchannels",
+    required: true
+  },
   goal: { type: Number, required: true, default: 0 }
 })
 
