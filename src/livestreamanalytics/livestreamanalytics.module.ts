ee0d32849b80edaa4f7ac085922899310d40d3b8
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2026-02-02 14:07:38 +0700

    fix: host revenue rankings (#134)

--- src/livestreamanalytics/livestreamanalytics.module.ts
+++ src/livestreamanalytics/livestreamanalytics.module.ts
@@ -4,12 +4,14 @@ import { LivestreamanalyticsController } from "./livestreamanalytics.controller"
 import { LivestreamanalyticsService } from "./livestreamanalytics.service"
 import { LivestreamSchema } from "../database/mongoose/schemas/Livestream"
 import { LivestreamMonthGoalSchema } from "../database/mongoose/schemas/LivestreamGoal"
+import { UserSchema } from "../database/mongoose/schemas/User"
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: "livestreams", schema: LivestreamSchema },
-      { name: "livestreammonthgoals", schema: LivestreamMonthGoalSchema }
+      { name: "livestreammonthgoals", schema: LivestreamMonthGoalSchema },
+      { name: "users", schema: UserSchema }
     ])
   ],
   controllers: [LivestreamanalyticsController],
