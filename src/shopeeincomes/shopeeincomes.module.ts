53f144a273e5e2c600e68a4c1a9dfdf36eb74e2b
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2026-04-10 10:47:03 +0700

    feat: shopee incomes (#178)

--- src/shopeeincomes/shopeeincomes.module.ts
+++ src/shopeeincomes/shopeeincomes.module.ts
@@ -3,15 +3,15 @@ import { MongooseModule } from "@nestjs/mongoose"
 import { ShopeeIncomesService } from "./shopeeincomes.service"
 import { ShopeeIncomesController } from "./shopeeincomes.controller"
 import { ShopeeIncomeSchema } from "../database/mongoose/schemas/ShopeeIncome"
+import { ShopeeChannelSchema } from "../database/mongoose/schemas/ShopeeChannel"
 import { ShopeeProductSchema } from "../database/mongoose/schemas/ShopeeProduct"
-import { LivestreamChannelSchema } from "../database/mongoose/schemas/LivestreamChannel"
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: "shopeeincomes", schema: ShopeeIncomeSchema },
-      { name: "ShopeeProduct", schema: ShopeeProductSchema },
-      { name: "livestreamchannels", schema: LivestreamChannelSchema }
+      { name: "shopeechannels", schema: ShopeeChannelSchema },
+      { name: "shopeeproducts", schema: ShopeeProductSchema }
     ])
   ],
   controllers: [ShopeeIncomesController],
