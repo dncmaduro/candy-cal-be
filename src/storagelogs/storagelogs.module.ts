011f8b1e9d070f2a2d910fd7ba1d2e8a8502a1f3
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2026-04-29 14:25:06 +0700

    feat: channel on slog (#192)

--- src/storagelogs/storagelogs.module.ts
+++ src/storagelogs/storagelogs.module.ts
@@ -5,12 +5,14 @@ import { StorageLogsController } from "./storagelogs.controller"
 import { StorageLogsService } from "./storagelogs.service"
 import { StorageItemSchema } from "../database/mongoose/schemas/StorageItem"
 import { SystemLogsModule } from "../systemlogs/systemlogs.module"
+import { DeliveredRequestSchema } from "../database/mongoose/schemas/DeliveredRequest"
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: "storagelogs", schema: StorageLogSchema },
-      { name: "storageitems", schema: StorageItemSchema }
+      { name: "storageitems", schema: StorageItemSchema },
+      { name: "deliveredrequests", schema: DeliveredRequestSchema }
     ]),
     SystemLogsModule
   ],
